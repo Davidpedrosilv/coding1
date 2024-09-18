e8fe9bc47c03fd357e3c67a9119417d288ec2ba0
PedroDk <Davidpedrosilv@icloud.com>
2024-09-18 13:54:17 +0000

    criação de front

---
+++ aula.js
@@ -0,0 +1,7 @@
+var prompt = require('prompt-sync') ();
+
+let lugar = prompt("de onde tu é irmão?");
+
+let nome = prompt(`e qual teu nome vei?`)
+console.log(`Então irmão, ${nome}!bem vindo vei, a Faculdade Senac`);
+
