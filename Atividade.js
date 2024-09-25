0ba27dfcc0f44b1d4792de269fca22f3260ef97e
PedroDk <Davidpedrosilv@icloud.com>
2024-09-25 11:11:10 -0300

    Criei o repositorio

---
+++ Atividade.js
@@ -0,0 +1,60 @@
+//Apresentação
+const nome = "Pedro"
+let idade = "28"
+console.log('Olá, meu nome é Pedro e tenho 28 anos');
+
+//Qual seu bairro e onde ele fica
+const bairro = "Pina"
+const cidade = "Recife"
+console.log('Pina e fica em Recife');
+
+//Calculos de variareis
+const numero1 = 20;
+const numero2 = 10;
+let soma = 10 + 20;
+let subtração = 20 - 10;
+let multiplicação = 10 * 20;
+let divisão = 20 / 10;
+
+console.log(soma);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+
+//Calculo de Aréa
+const base = "8";
+const altura = "5";
+const area = base * altura / 2;
+console.log(`A aréa do triângulo é ${area}`);
+
+//Calculo de Média
+const nota1 = "7";
+const nota2 = "5";
+const nota3 = "9";
+const media = (7 + 5 + 9) / 3;
+console.log(`O resultado da média é ${media}`);
+
+//Desconto
+var valor = (100);
+var percentualdesconto = (20);
+var valorfinal = valorfinal = valor - ( valor * (percentualdesconto / 100));
+console.log(`O valor total do produto com desconto é de ${valorfinal}$`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- aula.js
+++ aula.js
@@ -2,6 +2,6 @@ var prompt = require('prompt-sync') ();
 
 let lugar = prompt("de onde tu é irmão?");
 
-let nome = prompt(`e qual teu nome vei?`)
+let nome = prompt(`e qual teu nome vei?`);
 console.log(`Então ${nome}!bem vindo vei, a Faculdade Senac`);
 
